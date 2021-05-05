ae6289e1ce64677c3fad75dc3d6fae7ca7999d1a
Tai Nguyen <nguyentai2511myv@gmail.com>
2021-05-05 14:09:06 +0700

    make it better

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -101,7 +101,7 @@ class CartController extends Controller
         $total  = $products= DB::table('carts')
          ->join('products','carts.product_id','=','products.id')
          ->where('carts.user_id',$userId)
-         ->sum('products.price');
+         ->sum('price');
  
          return view('pages.checkout',['total'=>$total]);
     }
